62341452f5b3ba360d4a186a812795a05bec4dee
MariM-16 <69335919+MariM-16@users.noreply.github.com>
2021-04-20 21:40:46 -0400

    menu 2

---
+++ menu.py
@@ -0,0 +1,14 @@
+class Menu:
+    
+    def __init__(self,option):
+        self.__option = option
+
+    @property
+    def option(self):
+        return self.__option
+
+    def show_options(self, value):
+        if value < 0 :
+        
+
+
