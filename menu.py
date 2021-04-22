f7ce9c123809195a10e413100d7cde915fc3d3e6
MariM-16 <69335919+MariM-16@users.noreply.github.com>
2021-04-22 04:36:56 -0400

    algunas opciones menu

--- menu.py
+++ menu.py
@@ -7,9 +7,32 @@ class Menu:
     def option(self):
         return self.__option
 
-    def show_options(self, value):
-        if value < 0 :
+    def show_options_general(self, value):
+        print("\nSelect the type of action you want:")
+        print("1. Character action")
+        print("2. General action")
+        print("3. To challenge")
+        print('4. Counter')
+        #print("0. Salir") no se puede pasar turno
+        return int(input())
+    
+    def show_options_specific(self,value):
+        
+        if value == 1:
+            #aqui acciones de personajes
+            pass
+        elif value == 2:
+            print("\nSelect the type of action you want:")
+            print("1. Income")
+            print("2. Foreign aid")
+            print("3. Coup")
+        elif value == 3:
+            print("Challenge")
+            #aqui acciones de desafio 
+        elif value == 4:
+            print ("Counter")
+            # aca acciones de contraataque segun carta
+        else:
             print("malo")
-    
-    
-     
+            #aqui poner excepcion que elija correctamente
+        return int(input())
